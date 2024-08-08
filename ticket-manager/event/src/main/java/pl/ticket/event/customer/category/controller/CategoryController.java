d179b0f1083e35d75e75b88a030bbefd75bb794d
Krzysztof Kosmala <kkosmala9@gmail.com>
2024-08-08 15:16:09 +0200

    fixed(workaround) paggination in category controller

--- ticket-manager/event/src/main/java/pl/ticket/event/customer/category/controller/CategoryController.java
+++ ticket-manager/event/src/main/java/pl/ticket/event/customer/category/controller/CategoryController.java
@@ -3,12 +3,10 @@ package pl.ticket.event.customer.category.controller;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.ticket.event.common.model.Category;
 import pl.ticket.event.customer.category.dto.CategoryEventsDto;
 import pl.ticket.event.customer.category.service.CategoryService;
@@ -31,8 +29,9 @@ public class CategoryController {
     @GetMapping("/{slug}/event")
     public CategoryEventsDto getCategoriesWithEvents(@PathVariable
                                               @Pattern(regexp = "[a-z0-9\\-]+")
-                                              String slug, Pageable pageable) {
-
+                                              String slug, @RequestParam(defaultValue = "0") int page,
+                                                     @RequestParam(defaultValue = "5") int size) {
+        Pageable pageable = PageRequest.of(page, size);
         return categoryService.getCategoriesWithEvents(slug, pageable);
     }
 }
