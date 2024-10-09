b892dd6b518252d77e24b596a2c908d14213f2fe
neqmeister <neqmeister@gmail.com>
2024-10-09 16:27:35 +0200

    Filling the database with sample data

--- ticket-manager/cart/src/main/resources/database/2024-08-25/01-create-cart-item-table.sql
+++ ticket-manager/cart/src/main/resources/database/2024-08-25/01-create-cart-item-table.sql
@@ -3,7 +3,8 @@
 CREATE TABLE cart_item (
                            id BIGSERIAL PRIMARY KEY,
                            ticket_id INTEGER NOT NULL,
-                            cart_id INTEGER NOT NULL,
+                           cart_id INTEGER NOT NULL,
                            quantity INT,
+                           ticket_price NUMERIC(19, 2) NOT NULL,
                            CONSTRAINT fk_cart_item_cart_id FOREIGN KEY (cart_id) REFERENCES cart(id)
 );
