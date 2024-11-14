f600d591a4c487f2ddd2256218758182d0c0a5e3
Kamil Pawelec <neqmeister@gmail.com>
2024-11-14 19:27:24 +0100

    Fake temporary flow for payment

--- ticket-manager/payment/src/main/java/pl/ticket/payment/controller/PaymentController.java
+++ ticket-manager/payment/src/main/java/pl/ticket/payment/controller/PaymentController.java
@@ -2,6 +2,8 @@ package pl.ticket.payment.controller;
 
 import org.springframework.web.bind.annotation.*;
 import pl.ticket.dto.OrderEvent;
+import pl.ticket.payment.model.PaymentStatus;
+import pl.ticket.payment.service.PaymentOrderStatusService;
 import pl.ticket.payment.service.PaymentService;
 
 @RestController
@@ -9,13 +11,16 @@ import pl.ticket.payment.service.PaymentService;
 public class PaymentController {
 
     private final PaymentService paymentService;
+    private final PaymentOrderStatusService paymentOrderStatusService;
 
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService, PaymentOrderStatusService paymentOrderStatusService) {
         this.paymentService = paymentService;
+        this.paymentOrderStatusService = paymentOrderStatusService;
     }
 
     @PostMapping
-    public String initPayment(@RequestBody OrderEvent orderCreated){
+    public String initPayment(@RequestBody OrderEvent orderCreated) {
         return paymentService.initPayment(orderCreated);
     }
 }
+
--- ticket-manager/payment/src/main/java/pl/ticket/payment/service/p24/fakePayment/FakePayment24Service.java
+++ ticket-manager/payment/src/main/java/pl/ticket/payment/service/p24/fakePayment/FakePayment24Service.java
@@ -2,18 +2,35 @@ package pl.ticket.payment.service.p24.fakePayment;
 
 import org.springframework.stereotype.Service;
 import pl.ticket.dto.OrderEvent;
+import pl.ticket.payment.model.PaymentOrderStatus;
+import pl.ticket.payment.model.PaymentStatus;
+import pl.ticket.payment.repository.PaymentOrderStatusRepository;
+import pl.ticket.payment.service.PaymentOrderStatusService;
+
+import java.util.UUID;
 
 @Service
 public class FakePayment24Service implements PaymentInitializer {
 
-    @Override
-    public String initPayment(OrderEvent orderEvent) {
-        return "https://fake.sandbox.przelewy24.pl/" + orderEvent.hashCode();
+
+    private String generateFakeToken(){
+        return UUID.randomUUID().toString();
     }
 
     @Override
-    public boolean verifyPayment(OrderEvent orderEvent) {
-        // todo zrobic logike do fake statusu dla płatnosci
+    public String initPayment(OrderEvent orderEvent) {
+        return "https://fake.sandbox.przelewy24.pl/" + generateFakeToken();
+    }
+
+    @Override
+    public boolean verifyPayment(PaymentOrderStatus paymentOrderStatus) {
+        PaymentStatus paymentStatus = paymentOrderStatus.getPaymentStatus();
+
+        if (paymentStatus.equals(PaymentStatus.PAID)){
+            return true;
+        }else if(paymentStatus.equals(PaymentStatus.REJECTED)){
+            return false;
+        }
         return false;
     }
 }
